53f21405098c8a3aa1c758ff0f1291be0ebd399c
Yo <58520703+Yomiyou@users.noreply.github.com>
2020-03-14 02:10:59 +0100

    the first commit

---
+++ twentyfortyeight/src/twentyfortyeight/Grid.java
@@ -0,0 +1,30 @@
+package twentyfortyeight;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public final class Grid extends JPanel {
+
+   Grid() {
+       update();
+   }
+
+   public void update() {
+       this.repaint();
+   }
+
+   @Override
+   public void paintComponent(Graphics g) {
+       g.setColor(Color.BLACK);
+       g.drawRect(0, 0, 160, 160);
+       g.drawLine(40, 0, 40, 160);
+       g.drawLine(80, 0, 80, 160);
+       g.drawLine(120, 0, 120, 160);
+       g.drawLine(0, 40, 160, 40);
+       g.drawLine(0, 80, 160, 80);
+       g.drawLine(0, 120, 160, 120);
+   }
+
+}
+
